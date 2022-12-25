4fca32f4a907acc888c5787e27d596c5b74e3e88
Leonardo H. Felde <leonardohfelde@gmail.com>
2022-12-25 19:41:22 -0300

    ABAS e melhorias nas telas de loja e missões

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -4,18 +4,26 @@
       <v-row cols="12">
         <v-col cols="4">
           <StylizedCard brown-light content-class="d-flex" width="100%" height="80px" no-borders>
-            <span class="ma-auto caption grey--text"> foto agui </span>
+            <v-img
+          :src="require('../assets/img/penguino.png')"
+          class="my-3"
+          contain
+          height="61"
+        />
           </StylizedCard>
         </v-col>
         <v-col cols="8" class="text-start">
-          <span class="caption"> Oi, bem vindo a lojinha do Predo </span>
-          <div>
-            <span class="orange--text text--accent-2 caption"> 2 <v-icon small color="orange accent-2"> mdi-circle-multiple </v-icon></span>
+          <div class="caption mt-4">
+            Você possui<span class="orange--text text--accent-2 caption"> 2 <v-icon small color="orange accent-2"> mdi-circle-multiple </v-icon></span> <p />
+            Você comprou 0 items
           </div>
         </v-col>
       </v-row>
       <v-divider class="my-2"/>
-      <v-row cols="12">
+
+    <PixelTabs v-model="tab" :items="tabs" class="mb-3" content-class="d-flex justify-space-around"/>
+
+      <v-row cols="12" class="ma-0 mb-3">
         <v-col cols="4" v-for="i in 12" :key="i">
           <StylizedCard black content-class="d-flex" height="50px">
             <span class="ma-auto caption grey--text"> item </span>
@@ -27,11 +35,32 @@
   
 <script>
 // @ is an alias to /src
+
+import PixelTabs from '@/components/pixelTab.vue'
+
   
 export default {
   name: 'tasksIndex',
+
   components: {
-  }
+    PixelTabs
+  },
+    
+  data: () => ({
+    tab: 0,
+    tabs: [
+      { icon: 'mdi-sword' },
+      { icon: 'mdi-tshirt-crew' },
+      { icon: 'mdi-redhat' },
+      { icon: 'mdi-bottle-tonic-plus' },
+      { icon: 'mdi-paw' }
+    ]
+  }),
+  methods: {
+    changeTab() {
+      // ?
+    }
+  },
 }
 </script>
   
--- src/views/tasks/index.vue
+++ src/views/tasks/index.vue
@@ -1,26 +1,44 @@
 <template>
     <StylizedCard brown class="pa-2 ma-3">
       <v-card-title class="justify-center"> Missões </v-card-title>
-      <StylizedCard brown-light content-class="d-flex ma-5" height="60px" elevation="0">
-        <span class="ma-auto caption"> Oi, eu sou um teste </span>
-      </StylizedCard>
+
+      <PixelTabs big-tabs :items="tabs" v-model="tab" content-class="d-flex justify-space-around"/>
+
+      <!-- <StylizedCard brown-light content-class="d-flex ma-5" height="60px" elevation="0">
+        <span class="ma-auto caption"> testetete </span>
+      </StylizedCard> -->
 
       <v-divider class="my-2"/>
-
-      <QuestList :items="tasks" />
+      <v-card-subtitle class="justify-center pb-0"> Missões do {{ tabs[tab].description }}</v-card-subtitle>
+      <QuestList :items="tasks[tab]" />
 
     </StylizedCard>
   </template>
   
 <script>
 import QuestList from '@/components/QuestList.vue'
+import PixelTabs from '@/components/pixelTab.vue'
 
 export default {
   name: 'tasksIndex',
-  components: { QuestList },
+
+  components: {
+    QuestList,
+    PixelTabs
+  },
+
   data: () => ({
-    tasks: [{ id: 1, icon: 'mdi-sword', title: 'Task 1', xp: 20, money: 10, color: 'red' }, { id: 2, icon: 'mdi-sword', title: 'Task 2', xp: 20, money: 10, color: 'red'  }, { id: 3, icon: 'mdi-sword', title: 'Task 3', xp: 20, money: 10, color: 'red'  }],
-    
+    tasks: [
+      [{ id: 1, icon: 'mdi-arm-flex', title: 'Task 1', xp: 20, money: 10, color: 'red' }, { id: 2, icon: 'mdi-bike', title: 'Task 2', xp: 20, money: 10, color: 'red'  }, { id: 3, icon: 'mdi-run', title: 'Task 3', xp: 20, money: 10, color: 'red'  },],
+      [{ id: 1, icon: 'mdi-bookshelf', title: 'Task 1', xp: 20, money: 10, color: 'blue' }, { id: 2, icon: 'mdi-auto-fix', title: 'Task 2', xp: 20, money: 10, color: 'blue'  }, { id: 3, icon: 'mdi-head-snowflake-outline', title: 'Task 3', xp: 20, money: 10, color: 'blue'  },],
+      [{ id: 1, icon: 'mdi-bow-arrow', title: 'Task 1', xp: 20, money: 10, color: 'green' }, { id: 1, icon: 'mdi-forest', title: 'Task 1', xp: 20, money: 10, color: 'green' }]
+    ],
+    tab: 0,
+    tabs: [
+      { icon: 'mdi-sword', description: 'guerreiro' },
+      { icon: 'mdi-wizard-hat', description: 'mago' },
+      { icon: 'mdi-bow-arrow', description: 'ranger' }
+    ]
   }),
 }
 </script>
